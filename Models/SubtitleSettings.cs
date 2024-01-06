95712bf9d2fcef396341de1845330994e97751e9
Alex Schuepbach <alex.schupbach@student.nhlstenden.com>
2024-01-06 12:00:36 +0100

    Capitalized variable names

--- Models/SubtitleSettings.cs
+++ Models/SubtitleSettings.cs
@@ -3,10 +3,10 @@ namespace NetflixAPI.Models;
 public class Subtitle
 {
     public long Id { get; set; }
-    public long profileId { get; set; }
-    public long languageId { get; set; }
-    public required string size { get; set; }
-    public required string color { get; set; }
+    public long ProfileId { get; set; }
+    public long LanguageId { get; set; }
+    public required string Size { get; set; }
+    public required string Color { get; set; }
 
     public required Language Language { get; set; }
 }
