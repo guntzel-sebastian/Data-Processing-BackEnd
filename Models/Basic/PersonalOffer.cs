812659a21bb891c6e82acb39d8cdf2c6085511a3
Yohan Lozanov <yohan.lozanov@student.nhlstenden.com>
2024-01-09 13:52:50 +0100

    temporarily deleted files

--- Models/Basic/PersonalOffer.cs
+++
@@ -1,7 +0,0 @@
-namespace NetflixAPI.Models;
-
-public class PersonalOffer
-{
-    public long Id { get; set; }
-    public required HashSet<WatchableContent> Content { get; set; }
-}
--- Models/basic/ContentType.cs
+++
@@ -1,7 +0,0 @@
-namespace NetflixAPI.Models;
-
-public class ContentType
-{
-    public long Id { get; set; }
-    public required string Type { get; set; }
-}
