c0dda0d248405d4a7e83e6547202b4f33953a7ff
Alex Schuepbach <alex.schupbach@student.nhlstenden.com>
2024-01-10 16:21:50 +0100

    Added files back

---
+++ Models/Basic/Profile.cs
@@ -0,0 +1,40 @@
+public partial class Profile
+{
+    public Profile()
+    {
+        SubtitleSettings = new HashSet<SubtitleSettings>();
+
+        ProfileXContentTypePreferences = new HashSet<ProfileXContentTypePreference>();
+
+        ProfileXGenrePreferences = new HashSet<ProfileXGenrePreference>();
+
+        ProfileXClassificationPreferences = new HashSet<ProfileXClassificationPreference>();
+
+        WantToWatch = new HashSet<WantToWatch>();
+
+        ContentSession = new HashSet<ContentSession>();
+    }
+
+    public long ProfileId {get; set;}
+
+    public string Name {get; set;}
+
+    public string Photo {get; set;}
+
+    public string DateOfBirth {get; set;} // made string for now to avoid potential server conflicts
+
+    public long UserId {get; set;}
+
+    public virtual ICollection<SubtitleSettings> SubtitleSettings {get; set;}
+
+    public virtual ICollection<ProfileXContentTypePreference> ProfileXContentTypePreferences {get; set;}
+
+    public virtual ICollection<ProfileXGenrePreference> ProfileXGenrePreferences {get; set;}
+
+    public virtual ICollection<ProfileXClassificationPreference> ProfileXClassificationPreferences {get; set;}
+
+    public virtual ICollection<WantToWatch> WantToWatch {get; set;}
+
+    public virtual ICollection<ContentSession> ContentSession {get; set;}
+  
+}
---
+++ Models/Basic/Quality.cs
@@ -0,0 +1,16 @@
+namespace NetflixAPI.Models;
+
+public partial class Quality
+{
+    public Quality()
+    {
+        SubscriptionXQualityAlloweds = new HashSet<SubscriptionXQualityAllowed>();
+        EpisodeXQualityAvailables = new HashSet<EpisodeXQualityAvailable>();
+    }
+
+    public long QualityId {get; set;}
+    public string qualityName {get; set;}
+
+    public virtual ICollection<SubscriptionXQualityAllowed> SubscriptionXQualityAlloweds {get; set;}
+    public virtual ICollection<EpisodeXQualityAvailable> EpisodeXQualityAvailables {get; set;}
+}
