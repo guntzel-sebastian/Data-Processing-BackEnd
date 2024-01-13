5b6e79b731f29ff7b66e7cff35b2420dcc947473
Alex Schuepbach <alex.schupbach@student.nhlstenden.com>
2024-01-13 18:40:50 +0100

    Added models

--- Models/NetflixContext.cs
+++ Models/NetflixContext.cs
@@ -19,6 +19,18 @@ public class NetflixContext : DbContext
     public DbSet<Genre> Genre { get; set; } = null!;
     public DbSet<SubtitleSettings> SubtitleSettings { get; set; } = null!;
     public DbSet<SubtitleContent> SubtitleContent { get; set; } = null!;
+    public DbSet<Country> Country { get; set; } = null!;
+    public DbSet<FailedLoginAttempt> FailedLoginAttempt { get; set; } = null!;
+    public DbSet<Language> Language { get; set; } = null!;
+    public DbSet<Location> Location { get; set; } = null!;
+    public DbSet<Nationality> Nationality { get; set; } = null!;
+    public DbSet<PersonalOffer> PersonalOffer { get; set; } = null!;
+    public DbSet<Preference> Preference { get; set; } = null!;
+    public DbSet<Price> Price { get; set; } = null!;
+    public DbSet<Profile> Profile { get; set; } = null!;
+    public DbSet<Subscription> Subscription { get; set; } = null!;
+    public DbSet<TextItem> TextItem { get; set; } = null!;
+    public DbSet<Classification> Classification { get; set; } = null!;
 
 public DbSet<NetflixAPI.Models.WatchableContent> WatchableContent_1 { get; set; } = default!;
 
