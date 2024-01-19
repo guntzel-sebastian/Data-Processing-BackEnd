ea8407df1a3a713d9d4c3bcba0d09a62fcdfe67b
Alex Schuepbach <alex.schupbach@student.nhlstenden.com>
2024-01-19 12:33:40 +0100

    Reverted to default return

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -118,7 +118,7 @@ namespace NetflixAPI.Controllers
             _context.User.Add(user);
             await _context.SaveChangesAsync();
 
-            return Created();
+            return CreatedAtAction(nameof(GetUser), new { id = user.Id }, user);
         }
 
         [HttpPost]
