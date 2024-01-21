2dcc56fe0005fdff8ea979c709b61df575700ab9
Alex Schuepbach <alex.schupbach@student.nhlstenden.com>
2024-01-21 14:51:41 +0100

    Made models compatible with DB

--- Models/basic/FailedLoginAttempt.cs
+++ Models/basic/FailedLoginAttempt.cs
@@ -9,7 +9,7 @@ public partial class FailedLoginAttempt
     }
 
     public int user_id {get; set;}
-    public required string date {get; set;} // should be datetime probably
-    public required string time {get; set;} // should be merged with datetime
+    public required DateTime date {get; set;}
+    public required DateTime time {get; set;}
 
 }
